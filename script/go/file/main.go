0d30c47db3500edaefdd84e135abea24dabe4654
sunji <sunji@sunjideMacBook-Pro.local>
2021-07-02 18:53:43 +0800

    "ig:"

--- script/go/file/main.go
+++ script/go/file/main.go
@@ -1,5 +1,7 @@
 package main
 
+// https://colobu.com/2016/10/12/go-file-operations/
+
 import (
 	"io/ioutil"
 	"os"
@@ -26,7 +28,7 @@ func (tf *TheFile) Create() (*os.File, error) {
 }
 
 func (tf *TheFile) Exist() bool {
-	_, err := os.Stat(tf.path)
+	_, err := tf.Stat()
 
 	if err != nil {
 		if os.IsExist(err) {
@@ -50,4 +52,54 @@ func (tf *TheFile) WriteSimple(bs []byte) error {
 }
 func (tf *TheFile) ReadSimple() ([]byte, error) {
 	return ioutil.ReadFile(tf.path)
+}
+
+func (tf *TheFile) Stat() (os.FileInfo, error) {
+	return os.Stat(tf.path)
+	// stat, err := os.Stat(tf.path)
+	// return stat, err
+}
+
+func (tf *TheFile) Mv(new string) error {
+	old := tf.path
+	return os.Rename(old, new)
+}
+
+func (tf *TheFile) Open(flag int, mode os.FileMode) (*os.File, error) {
+	// flag:
+	//   os.O_RDONLY // 只读
+	//   os.O_WRONLY // 只写
+	//   os.O_RDWR // 读写
+	//   os.O_APPEND // 往文件中添建（Append）
+	//   os.O_CREATE // 如果文件不存在则先创建
+	//   os.O_TRUNC // 文件打开时裁剪文件
+	//   os.O_EXCL // 和O_CREATE一起使用，文件不能存在
+	//   os.O_SYNC // 以同步I/O的方式打开
+
+	// mode: 如0666
+	//   d=isdir, u=own, g=group, o=other
+	//   r = 4
+	//   w = 2
+	//   x = 1
+	//   rwx = 4 + 2 + 1 = 7
+	//   rw = 4 + 2 = 6
+	//   rx = 4 +1 = 5
+
+	// os.Open(name) == OpenFile(name, O_RDONLY, 0)  只读
+
+	// os.Create(name) == OpenFile(name, O_RDWR|O_CREATE|O_TRUNC, 0666)  读|写|空|创建
+
+	return os.OpenFile(tf.path, flag, mode)
+}
+
+func (tf *TheFile) Chmod(mode os.FileMode) error {
+	return os.Chmod(tf.path, mode)
+}
+
+func (tf *TheFile) Chown(uid, gid int) error {
+	return os.Chown(tf.path, uid, gid)
+}
+
+func (tf *TheFile) Symlink(target string) error {
+	return os.Symlink(tf.path, target)
 }
--- script/go/file/main_test.go
+++ script/go/file/main_test.go
@@ -2,63 +2,101 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func Test_File(t *testing.T) {
-	Convey("", t, func() {
+func Test_Create(t *testing.T) {
+	Convey("create empty file", t, func() {
 
 		o := New(tmp_file)
 
-		Convey("create empty file", func() {
-			Convey("file not exist", func() {
-				So(o.Exist(), ShouldBeFalse)
-				Convey("create()", func() {
-					_, err := o.Create()
-					So(err, ShouldBeNil)
-					Convey("file exist", func() {
-						So(o.Exist(), ShouldBeTrue)
+		Convey("file not exist", func() {
+			So(o.Exist(), ShouldBeFalse)
+			Convey("create()", func() {
+				_, err := o.Create()
+				So(err, ShouldBeNil)
+				Convey("file exist", func() {
+					So(o.Exist(), ShouldBeTrue)
+				})
+			})
+		})
+		defer o.Remove()
+	})
+}
+
+func Test_Truncate(t *testing.T) {
+	Convey("truncate file content", t, func() {
+
+		o := New(tmp_file)
+
+		content := "abcdefg"
+		Convey("init file -> "+content, func() {
+			err := o.WriteSimple([]byte(content))
+			So(err, ShouldBeNil)
+			Convey(fmt.Sprintf("truncate(3) %s -> abc", content), func() {
+				err = o.Truncate(3)
+				So(err, ShouldBeNil)
+				bs, err := o.ReadSimple()
+				So(err, ShouldBeNil)
+				So(bs, ShouldResemble, []byte("abc"))
+
+			})
+			Convey(fmt.Sprintf("truncate(10) %s -> %s<nil><nil><nil>", content, content), func() {
+				err = o.Truncate(10)
+				So(err, ShouldBeNil)
+				bs, err := o.ReadSimple()
+				So(err, ShouldBeNil)
+				// t.Log(bs, len(bs), string(bs))
+				rs := append([]byte(content), 0, 0, 0)
+				So(bs, ShouldResemble, rs)
+			})
+
+			Convey(fmt.Sprintf("truncate(0) %s -> ", content), func() {
+				err = o.Truncate(0)
+				So(err, ShouldBeNil)
+				bs, err := o.ReadSimple()
+				So(err, ShouldBeNil)
+				So(bs, ShouldBeEmpty)
+			})
+		})
+
+		defer o.Remove()
+	})
+}
+
+func Test_Permission(t *testing.T) {
+	Convey("check permission", t, func() {
+
+		o := New(tmp_file)
+
+		Convey("create file", func() {
+			_, err := o.Open(os.O_CREATE|os.O_RDONLY|os.O_TRUNC, 0666)
+			So(err, ShouldBeNil)
+
+			Convey("chmod rw -> r", func() {
+				err := o.Chmod(0444)
+				So(err, ShouldBeNil)
+				Convey("check permission write", func() {
+					_, err := o.Open(os.O_WRONLY, 0666)
+					So(os.IsPermission(err), ShouldBeTrue)
+
+					Convey("chown", func() {
+						err := o.Chown(os.Getuid(), os.Getgid())
+						// TODO 如何获取文件的own, group?
+						So(err, ShouldBeNil)
 					})
 				})
 			})
-			defer o.Remove()
 		})
+		defer o.Remove()
+	})
+}
 
-		Convey("truncate", func() {
-			content := "abcdefg"
-			Convey("init file -> "+content, func() {
-				err := o.WriteSimple([]byte(content))
-				So(err, ShouldBeNil)
-				Convey(fmt.Sprintf("truncate(3) %s -> abc", content), func() {
-					err = o.Truncate(3)
-					So(err, ShouldBeNil)
-					bs, err := o.ReadSimple()
-					So(err, ShouldBeNil)
-					So(bs, ShouldResemble, []byte("abc"))
+func Test_SymLink(t *testing.T) {
+	Convey("", t, func() {
 
-				})
-				Convey(fmt.Sprintf("truncate(10) %s -> %s<nil><nil><nil>", content, content), func() {
-					err = o.Truncate(10)
-					So(err, ShouldBeNil)
-					bs, err := o.ReadSimple()
-					So(err, ShouldBeNil)
-					// t.Log(bs, len(bs), string(bs))
-					rs := append([]byte(content), 0, 0, 0)
-					So(bs, ShouldResemble, rs)
-				})
-
-				Convey(fmt.Sprintf("truncate(0) %s -> ", content), func() {
-					err = o.Truncate(0)
-					So(err, ShouldBeNil)
-					bs, err := o.ReadSimple()
-					So(err, ShouldBeNil)
-					So(bs, ShouldBeEmpty)
-				})
-			})
-
-			defer o.Remove()
-		})
 	})
 }
