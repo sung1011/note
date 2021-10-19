c23b99d50e8464d20bc939333490388e97e0270e
sunji <sunji@sunjideMacBook-Pro.local>
2021-10-19 21:09:48 +0800

    "ig:"

--- script/go/package/bufio_test.go
+++ script/go/package/bufio_test.go
@@ -46,14 +46,26 @@ func Test_Bufio_Read(t *testing.T) {
 func Test_Bufio_Write(t *testing.T) {
 	Convey("", t, func() {
 		bf := new(bytes.Buffer) // writer
-		w := bufio.NewWriter(bf)
-		w.WriteString("a") // 写入w的缓冲
-		w.WriteString("b")
-		w.WriteString("c")
-		So(w.Buffered(), ShouldEqual, 3)
-		w.Flush() // 将w的缓冲写入writer
 
-		So(w.Buffered(), ShouldEqual, 0) // w的缓冲区已经被清空
-		So(bf.Len(), ShouldEqual, 3)     // 确实已经写入writer了
+		w1 := bufio.NewWriter(bf) // 新建缓冲区
+		w1.WriteString("a")       // 写入w1的缓冲
+		w1.WriteString("b")
+		w1.WriteString("c")
+		t.Log(w1.Size())                  // defaultBufSize=4096
+		So(w1.Buffered(), ShouldEqual, 3) // 没有flush, 数据还在w1的缓冲区
+		So(bf.Len(), ShouldEqual, 0)      // 没有flush, 所以尚未写入bf
+
+		newbf := new(bytes.Buffer)
+		w1.Reset(newbf)
+		So(w1.Buffered(), ShouldEqual, 0) // reset清空缓冲区, 并装载新的bf
+
+		w2 := bufio.NewWriterSize(bf, w1.Size()*2) // 新建缓冲区(by 指定size)
+		w2.WriteString("x")                        // 写入w的缓冲
+		w2.WriteString("y")
+		So(bf.Len(), ShouldEqual, 0) // 尚未写入bf
+		w2.Flush()                   // 将w的缓冲写入bf
+
+		So(w2.Buffered(), ShouldEqual, 0) // w2的缓冲区已经被清空
+		So(bf.Len(), ShouldEqual, 2)      // 确实已经写入bf了
 	})
 }
